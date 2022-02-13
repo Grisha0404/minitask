bf72da18b1e0ae8d4e28e5470b23b11d96ff4b91
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-02-13 15:37:40 +0300

    Update NewComponent.tsx

--- src/NewComponent.tsx
+++ src/NewComponent.tsx
@@ -1,52 +1,39 @@
 import React from 'react';
+import {FilterType, StudentType, TopCarsType} from "./App";
+import Buttons from "./Buttons";
+import Input from "./Input";
+import TopCars from "./TopCars";
 
 type NewComponentType = {
     students: StudentType[]
     topCars: TopCarsType[]
-}
-type StudentType = {
-    id: number,
-    name: string,
-    age: number
+    removeStudents:( id:number )=>void
+    changeStudents:(filter: FilterType) => void
+    addNewStudent: (newStudent:string)=> void
+    addNewCars: (cars:string)=>void
 }
 
-type TopCarsType = {
-    manufacturer: string,
-    model: string
-}
-export const topCar = [
-    {manufacturer: 'BMW', model: 'm5cs'},
-    {manufacturer: 'Mercedes', model: 'ml'},
-    {manufacturer: 'Opel', model: 'cadet'}
-]
-export const NewComponent = (props: NewComponentType, index: number) => {
 
-    const foo1 = () =>{
+export const NewComponent = (props: NewComponentType) => {
+   /* const foo1 = () => {
         console.log('100200')
     }
-    const foo2 = (name:string) => {
+    const foo2 = (name: string) => {
         console.log(name)
-    }
+    }*/
+    const studentsHandler = props.students.map(s => <li key={s.id}><span>{s.name}</span> <span>{s.age}</span>
+        <button onClick={()=>props.removeStudents(s.id)}>x</button></li>)
 
     return (
         <div>
-            {/*<table>
-                <tr>
-                    <th>Info</th>
-                    <th>Model</th>
-                </tr>
-                <tr>
-                    <td>{props.topCars.map(s => <li key={index}>{s.manufacturer}</li>)}</td>
-                    <td>{props.topCars.map(m => <li key={index}> {m.model}</li>)}</td>
-                </tr>
-            </table>
+            <TopCars topCars={props.topCars} addNewCars={props.addNewCars}/>
+            <Input addNewStudent={props.addNewStudent}/>
             <ul>
-                {
-                    props.students.map(s => <li key={s.id}>{s.name}-{s.age}</li>)
-                }
-            </ul>*/}
-            <button onClick={foo1}>1</button>
-            <button onClick={()=> foo2("100200")}>2</button>
+                {studentsHandler}
+            </ul>
+            <Buttons changeStudents={props.changeStudents}/>
+            {/*<button onClick={foo1}>1</button>
+            <button onClick={() => foo2("100200")}>2</button>*/}
         </div>
     );
 };
